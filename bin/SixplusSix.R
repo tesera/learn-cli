93461bcd75af27ae8ccd68ca37c333376768b852
Yves Richard <yves.richard@tesera.com>
2015-11-21 10:12:26 -0700

    Remove unessesary files

--- bin/SixplusSix.R
+++
@@ -1,9 +0,0 @@
-setwd("/opt/MRAT_Refactor/Rwd")
-source(paste("/opt/MRAT_Refactor/etc/", "XIterativeVarSel.R.conf", sep = ""))
-#testVar <- 6 + 6
-#source(paste("/opt/MRAT_Refactor/bin/test_XIterativeVarSelCorVarElimination.R"))
-source("/opt/MRAT_Refactor/Rwd/RScript/ZLoadDatasetAndAttachVariableNames.R")
-step1 <- 3.1
-write.csv(step1, file = "step1.csv")
-testVar <- 6 + 6
-
