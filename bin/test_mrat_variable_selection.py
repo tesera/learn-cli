0b464f9871b84741abe02b37c8a07aebc962c2cf
Boris Cosic <boris.cosic@tesera.com>
2015-12-01 14:14:56 -0700

    Fixes to DAVS library.

--- bin/test_mrat_variable_selection.py
+++ bin/test_mrat_variable_selection.py
@@ -46,16 +46,14 @@ class mrat_variable_selection(object):
         ranksVariables = RANKVAR.RankVar()
         
         # test2_XIterativeVarSelCorVarElimination.R runs steps 13 -18 ZCompleteVariableSelectionPlusRemoveCorrelationVariables.R
-        #r.source("/opt/MRAT_Refactor/bin/test2_XIterativeVarSelCorVarElimination.R")        
         davs.vs_CompleteVariableSelectionPlusRemoveCorrelationVariables(r['lviFileName'], r['uniqueVarPath'], 'UCORCOEF.csv')        
         
         removeCorXVars = REMOVE_HIGHCORVAR_FROM_XVARSELV.Remove_HighCorVar_from_XVarSelv()
         nextCount = COUNT_XVAR_IN_XVARSELV1.Count_XVar_in_XVarSelv1()
-        print "currentCount = ", currentCount, "  nextCount = ", nextCount
-        print "\n"
+        flog.flog_info("CurrentCount = %s, nextCount %s", currentCount, nextCount)
         
         # "Kluge" is: Helps get around an unknown crash when calling test3_XiterativeVarSelCorVarElimination.R
-        r.source("/opt/MRAT_Refactor/bin/kluge.R") 
+        #r.source("/opt/MRAT_Refactor/bin/kluge.R") 
         
         # LOOP THROUGH THE AUTOMATED VARIABLE SELECTION PROCESS TO ELIMINATE VARIABLES THAT DO NOT CONTRIBUTE TO THE MODEL
         counter = 0
