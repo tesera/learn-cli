4778c3ac3177a6593dbe67bcaca5b7904e4e0180
Yves Richard <yves.richard@tesera.com>
2016-02-12 09:32:51 -0700

    Add example folder
    see readme to run.

---
+++ example/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+python ./mrat.py \
+  s3://tesera.svc.variable-selection/uploads/22a7f809-b28e-4ff5-bb05-39c126e45605/ANALYSIS.csv \
+  s3://tesera.svc.variable-selection/uploads/22a7f809-b28e-4ff5-bb05-39c126e45605/XVARSELV1.csv \
+  s3://tesera.svc.variable-selection/uploads/22a7f809-b28e-4ff5-bb05-39c126e45605/output/1 \
+  --classVariableName CLASS5 \
+  --excludeRowValue -1 \
+  --excludeRowVarName SORTGRP \
+  --minNvar 1 \
+  --maxNvar 20 \
+  --nSolutions 20 \
+  --criteria xi2
