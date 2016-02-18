ad76ca357d1f3a53d8bb1e75951877a81dfc59e8
Yves Richard <yves.richard@tesera.com>
2016-02-18 16:00:42 -0700

    Update s3 example url

--- example/run-s3.sh
+++ example/run-s3.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 varselect \
-  s3://tesera.svc.variable-selection/uploads/22a7f809-b28e-4ff5-bb05-39c126e45605/ANALYSIS.csv \
-  s3://tesera.svc.variable-selection/uploads/22a7f809-b28e-4ff5-bb05-39c126e45605/XVARSELV1.csv \
-  s3://tesera.svc.variable-selection/uploads/22a7f809-b28e-4ff5-bb05-39c126e45605/output/1 \
+  s3://tesera.svc.variable-selection/uploads/example/ANALYSIS.csv \
+  s3://tesera.svc.variable-selection/uploads/example/XVARSELV1.csv \
+  s3://tesera.svc.variable-selection/uploads/example/output/1 \
   --classVariableName CLASS5 \
   --excludeRowValue -1 \
   --excludeRowVarName SORTGRP \
