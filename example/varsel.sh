945a6a6aa49f5439b048ba1cc7b64a9137bd653e
Yves Richard <yves.richard@tesera.com>
2016-03-31 14:09:03 -0600

    Update varsel example with new file

--- example/varsel.sh
+++ example/varsel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 learn varsel \
-    --xy-data ./example/ANALYSIS.csv \
-    --config ./example/XVARSELV1.csv \
+    --xy-data ./example/data_xy.csv \
+    --config ./example/config_vsel.csv \
     --output ./example/varsel \
     --yvar CLPRDP
