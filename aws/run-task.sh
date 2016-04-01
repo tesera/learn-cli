206356bb2bd53a1e9617b648847e007c8bbcb5f0
Yves Richard <yves.richard@tesera.com>
2016-04-01 16:46:28 -0600

    Add command specific container overrides

--- aws/run-task.sh
+++ aws/run-task.sh
@@ -3,5 +3,5 @@
 aws ecs run-task \
     --cluster learn \
     --task-definition learn \
-    --overrides file://overrides.json \
+    --overrides file://overrides-${1-varsel}.json \
     --started-by task:learn
