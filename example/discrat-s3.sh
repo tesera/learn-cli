1bc8f778bc7620e7521c0c529fe734613ee9c93b
Yves Richard <yves.richard@tesera.com>
2016-04-05 14:57:04 -0600

    Update examples and aws overrides

--- example/discrat-s3.sh
+++ example/discrat-s3.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 learn discrat \
-    --xy-data s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459878060/filter/data_xy.csv \
-    --x-data s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459878060/filter/data_x_filtered.csv \
-    --dfunct s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459878060/lda/DFUNCT.csv \
-    --idf s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459878060/data_idf.csv \
+    --xy-data s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459887448/filter/data_xy.csv \
+    --x-data s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459887448/filter/data_x_filtered.csv \
+    --dfunct s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459887448/lda/DFUNCT.csv \
+    --idf s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459887448/data_idf.csv \
     --varset 18 \
-    --output s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459878060/discrat
+    --output s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459887448/discrat
--- example/varsel-s3.sh
+++ example/varsel-s3.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 learn varsel \
-    --xy-data s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459878060/filter/data_xy.csv \
-    --config s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459878060/filter/vsel_xy_config.csv \
-    --output s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459878060/varsel
+    --xy-data s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459887448/filter/data_xy.csv \
+    --config s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459887448/filter/vsel_xy_config.csv \
+    --output s3://tesera.svc.learn/organizations/tesera/projects/example/scenarios/20150116-1459887448/varsel
