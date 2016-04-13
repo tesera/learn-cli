f933de24502c7339c9e059db62c50fdc27d5637f
Yves Richard <yves.richard@tesera.com>
2016-04-13 12:32:13 -0600

    Refactor lda client
    Same as varselect client its not been tested and not working. Waiting on rlearn refactor to be completed.

--- learn/clients/analyze.py
+++ learn/clients/analyze.py
@@ -1,60 +1,59 @@
-import os
 import logging
 
 import pandas as pd
-
+import pandas.rpy.common as com
 import rpy2.robjects as robjects
-from rpy2.robjects.packages import STAP
 from rpy2.robjects.packages import importr
 
-from pylearn.ldanalysis.CohensKhat import CohensKhat
-from pylearn.ldanalysis.CombineEvaluationDatasets import CombineEvaluationDatasets
-from pylearn.ldanalysis.varset import get_xvars, get_rloadrank, get_avgp
+from pylearn.lda import cohens_khat, combine_evaluation_datasets
 
 logger = logging.getLogger('pylearn')
+importr('MASS')
+r = robjects.r
+rlearn = importr('rlearn')
+
+def analyze(xy, config, yvar):
+    r_xy = com.convert_to_r_dataframe(xy)
+    r_config = com.convert_to_r_dataframe(vsel_xy_config)
+
+    args = [
+        r_xy,
+        r_config,
+        'SORTGRP',
+        -1,
+        yvar,
+        'SAMPLE'
+    ]
+
+    rlearn.lda(**args)
+
 
 class Analyze(object):
-    importr('MASS')
-
-    def __init__(self):
-        self.analyze = importr('rlearn').lda_RunLinearDiscriminantVariableAnalysis
 
     def run(self, args):
         outdir = args['--output']
         yvar = args['--yvar']
 
+        xy = pd.read_csv(args['--xy-data'])
+        config = pd.read_csv(args['--config'])
+
         logger.info(':lda: running lda analyze...')
-        aargs = [
-            args['--xy-data'],
-            args['--config'],
-            'SORTGRP',
-            -1,
-            yvar,
-            'SAMPLE',
-            outdir
-        ]
+        analyze(xy, config, yvar)
 
-        self.analyze(*aargs)
+        prior = com.load_data('lda.prior')
+        ctabulation = com.load_data('lda.ctabulation')
+        posterior = com.load_data('lda.posterior')
+        ctball = com.load_data('lda.ctball')
+        varmeans = com.load_data('lda.varmeans')
+        dfunct = com.load_data('lda.dfunct')
+        bwratio = com.load_data('lda.bwratio')
 
-        logger.info(':lda: running CohensKhat...')
-        cohens_khat = CohensKhat(outdir)
-        cohens_khat.run()
+        logger.info(':lda: applying Cohens Khat and writing to ctabsum')
+        ctabsum = cohens_khat(ctabulation)
 
-        logger.info(':lda: running CombineEvaluationDatasets...')
-        combine_evaluation = CombineEvaluationDatasets(outdir)
-        combine_evaluation.run()
+        logger.info(':lda: combininig evaluation datasets into assess')
+        assess = combine_evaluation_datasets(ctabsum, posterior, config)
 
-        xy_reference_path = os.path.join(outdir, 'ANALYSIS.csv')
-        x_var_dfunct_path = os.path.join(outdir, 'DFUNCT.csv')
+        assess.to_csv(os.path(outdir, 'lda_x_assess.csv'))
 
-        xy = pd.read_csv(xy_reference_path)
-        dfunct = pd.read_csv(x_var_dfunct_path)
-
-        logger.info(':lda: exporting relative loading ranks for all unique xvars in varsets (rloadrank)')
-        rloadrank = get_rloadrank(xy=xy, dfunct=dfunct, yvar=yvar)
-        rloadrank.to_csv(os.path.join(outdir, 'RLOADRANK.csv'))
-
-        logger.info(':lda: exporting an average p file per unique xvar in varsets')
-        all_xvars = get_xvars(dfunct)
-        avgp = get_avgp(xy, all_xvars)
-        avgp.to_csv(os.path.join(outdir, 'AVGP.csv'))
+        #TODO: eval if we really need to serialize avgp and rloadrank
