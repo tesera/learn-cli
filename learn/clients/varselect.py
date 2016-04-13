d5c7026356e944e7c76e0781664cd4e2d06cc721
Yves Richard <yves.richard@tesera.com>
2016-04-13 12:07:51 -0600

    Refactor varselect client
    Non tested/working stub/contract for new varselect client with pylearn/rlearn v2.0.

--- learn/clients/varselect.py
+++ learn/clients/varselect.py
@@ -1,97 +1,92 @@
 import logging
 
+import pandas as pd
+import pandas.rpy.common as com
 import rpy2.robjects as robjects
-from rpy2.robjects.packages import STAP
 from rpy2.robjects.packages import importr
 
-from pylearn.varselect.count_xvar_in_xvarsel import CountXVarInXvarSel
-from pylearn.varselect.test_extract_rvariable_combos import ExtractRVariableCombos
-from pylearn.varselect.rank_var import RankVar
-from pylearn.varselect.remove_highcorvar_from_xvarsel import RemoveHighCorVarFromXVarSel
+from pylearn.varselect import count_xvars, rank_xvars, extract_xvar_combos, remove_high_corvar
 
 logger = logging.getLogger('pylearn')
+importr('subselect')
+r = robjects.r
+rlearn = importr('rlearn')
+
+def organize(xy, config):
+    r_xy = com.convert_to_r_dataframe(xy)
+    r_config = com.convert_to_r_dataframe(vsel_xy_config)
+
+    rlearn.organize(r_xy, r_config)
+
+    return com.load_data('vs.varselect')
+
+def complete(xy, uniquevar):
+    r_xy = com.convert_to_r_dataframe(xy)
+    r_uniquevar = com.convert_to_r_dataframe(uniquevar)
+
+    rlearn.complete(r_xy, r_uniquevar)
+
+    varselect = com.load_data('vs.varselect')
+    return com.load_data('vs.ucorcoef')
+
+def varselect(self, xy, config):
+
+    # Identify and organize variable sets (varsets)
+    varselect = organize(xy, config)
+
+    # Identifies the unique variable sets
+    vsel_x, vsel_uniquevar = extract_xvar_combos(varselect)
+
+    # Rank the variables in terms of their contribution to a model
+    varrank = rank_xvars(varselect)
+
+    # Complete var select and remove correlation variables
+    ucorcoef = complete(xy, uniquevar)
+
+    # Removes variables which have a high correlation
+    config = remove_high_corvar(ucorcoef, vsel_varrank)
+
+    return config
+
 
 class VarSelect(object):
-    importr('subselect')
-
-    def __init__(self):
-        self.flog = importr('futile.logger')
-        self.r = robjects.r
-
-    def init(self, args):
-        self.count_xvars = CountXVarInXvarSel(args['--output']).count
-        self.rank_xvars = RankVar(args['--output']).rank
-        self.extract_rvariable_combos = ExtractRVariableCombos(args['--output']).extract_rvariable_combos
-        self.remove_highcorvar = RemoveHighCorVarFromXVarSel(args['--output']).remove_high_cor_vars
-
-        self.rlearn = importr('rlearn')
-
-        self.r('classVariableName <- "%s"' % args['--yvar'])
-        self.r('excludeRowValue <- "%s"' % -1)
-        self.r('excludeRowVarName <- "SORTGRP"')
-        self.r('xVarSelectCriteria <- "X"')
-        self.r('minNvar <- %d' % int(args['--minNvar']))
-        self.r('maxNvar <- %d' % int(args['--maxNvar']))
-        self.r('nSolutions <- %d' % int(args['--nSolutions']))
-        self.r('criteria <- "%s"' % args['--criteria'])
-
-        self.r('lviFileName <- "%s"' % args['--xy-data'])
-        self.r('outDir <- "%s"' % args['--output'])
-        self.r('xVarSelectFileName <- "%s"' % args['--config'])
-        self.r('uniqueVarPath <- "%s/UNIQUEVAR.csv"' % args['--output'])
-        self.r('printFileName <- "%s/UCORCOEF.csv"' % args['--output'])
-        self.r('xVarCountFileName <- "%s/XVARSELV1_XCOUNT.csv"' % args['--output'])
-        self.r('varSelect <- "%s/VARSELECT.csv"' % args['--output'])
-
-
-    def varselect(self):
-
-        # Input: data_xy.csv, vsel_xy_config.csv
-        # Output: VARSELECT.csv
-        self.rlearn.vs_IdentifyAndOrganizeUniqueVariableSets(self.r['lviFileName'], self.r['xVarSelectFileName'], self.r['varSelect'])
-
-        # Identifies the unique variable sets
-        # Input: VARSELECT.csv
-        # Output: vsel_x.csv, vsel_uniquevar.csv
-        self.extract_rvariable_combos()
-
-        # Rank the variables in terms of their contribution to a model
-        # Input: VARSELECT.csv
-        # Output: vsel_varrank.csv (VARRANK.csv)
-        self.rank_xvars()
-
-        # Input: data_xy.csv, vsel_uniquevar
-        # Output: UCORCOEF.csv
-        self.rlearn.vs_CompleteVariableSelectionPlusRemoveCorrelationVariables(self.r['lviFileName'], self.r['uniqueVarPath'], self.r['printFileName'])
-
-        # Removes variables which have a high correlation
-        # Input: UCORCOEF.csv, VARRANK.csv
-        # Output: vsel_xy_config.csv
-        self.remove_highcorvar()
-
 
     def run(self, args):
         logger.info("Starting variable_selection")
-        args['--nSolutions'], args['--minNvar'], args['--maxNvar'] = args['--iteration'].split(':')
-        self.init(args)
 
-        current_nxvar = self.count_xvars()
-        self.r('current_nxvar <- %d' % current_nxvar)
+        args['--nSolutions'], args['--minNvar'], args['--maxNvar'] = args['--iteration'].split(':')
+
+        # TODO: work in these args into the mix
+        # r('classVariableName <- "%s"' % args['--yvar'])
+        # r('excludeRowValue <- "%s"' % -1)
+        # r('excludeRowVarName <- "SORTGRP"')
+        # r('xVarSelectCriteria <- "X"')
+        # r('minNvar <- %d' % int(args['--minNvar']))
+        # r('maxNvar <- %d' % int(args['--maxNvar']))
+        # r('nSolutions <- %d' % int(args['--nSolutions']))
+        # r('criteria <- "%s"' % args['--criteria'])
+
+        xy = pd.read_csv(args['--xy-data'])
+        config = pd.read_csv(args['--config'])
+
+        current_nxvar = self.count_xvars(config)
+        r('current_nxvar <- %d' % current_nxvar)
         logger.info("Initial variable count: %s", current_nxvar)
 
-        self.varselect()
+        config = varselect(xy, config)
 
-        next_nxvar = self.count_xvars()
-        self.r('xVarCount <- %d' % next_nxvar)
+        next_nxvar = self.count_xvars(config)
+        r('xVarCount <- %d' % next_nxvar)
         logger.info("CurrentCount = %s, nxvar %s", current_nxvar, next_nxvar)
 
+        # Run variable selection while model improves
         iteration = 0
         while current_nxvar != next_nxvar:
             current_nxvar = next_nxvar
 
-            self.varselect()
+            config = varselect(xy, config)
 
-            next_nxvar = self.count_xvars()
+            next_nxvar = self.count_xvars(config)
             iteration = iteration + 1
 
         logger.info("Number of Iterations: %s", iteration)
