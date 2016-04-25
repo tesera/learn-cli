2f6c813c7b021426ae367335f5c70239f33d5f06
Yves Richard <yves.richard@tesera.com>
2016-04-24 20:08:12 -0600

    Fix lda client
    Interim clients. Todos still to fix.

--- learn/clients/analyze.py
+++ learn/clients/analyze.py
@@ -1,7 +1,10 @@
+import os
 import logging
 
 import pandas as pd
-import pandas.rpy.common as com
+from rpy2.robjects import pandas2ri
+pandas2ri.activate()
+
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 
@@ -9,23 +12,24 @@ from pylearn.lda import cohens_khat, combine_evaluation_datasets
 
 logger = logging.getLogger('pylearn')
 importr('MASS')
+importr('logging')
 r = robjects.r
 rlearn = importr('rlearn')
 
-def analyze(xy, config, yvar):
-    r_xy = com.convert_to_r_dataframe(xy)
-    r_config = com.convert_to_r_dataframe(vsel_xy_config)
+def analyze(xy, config, yvar, output):
 
     args = [
-        r_xy,
-        r_config,
+        xy,
+        config,
         'SORTGRP',
         -1,
         yvar,
-        'SAMPLE'
+        False,
+        output,
+        True
     ]
 
-    rlearn.lda(**args)
+    rlearn.lda(*args)
 
 
 class Analyze(object):
@@ -38,27 +42,23 @@ class Analyze(object):
         config = pd.read_csv(args['--config'])
 
         logger.info(':lda: running lda analyze...')
-        analyze(xy, config, yvar)
+        analyze(xy, config, yvar, outdir)
 
-        outputs = {
-            'prior': com.load_data('lda.prior'),
-            'ctabulation': com.load_data('lda.ctabulation'),
-            'posterior': com.load_data('lda.posterior'),
-            'ctball': com.load_data('lda.ctball'),
-            'varmeans': com.load_data('lda.varmeans'),
-            'dfunct': com.load_data('lda.dfunct'),
-            'bwratio': com.load_data('lda.bwratio')
-        }
+        ctabulation = pd.read_csv(os.path.join(outdir, 'CTABULATION.csv'))
+        posterior = pd.read_csv(os.path.join(outdir, 'POSTERIOR.csv'), index_col=['VARSET'])
+        dfunct = pd.read_csv(os.path.join(outdir, 'DFUNCT.csv'))
 
         logger.info(':lda: applying Cohens Khat and writing to ctabsum')
-        outputs.ctabsum = cohens_khat(outputs.ctabulation)
+        ctabsum = cohens_khat(ctabulation)
 
         logger.info(':lda: combininig evaluation datasets into assess')
-        assess = combine_evaluation_datasets(outputs.ctabsum, outputs.posterior, config)
+        assess = combine_evaluation_datasets(ctabsum, posterior, config)
 
-        assess.to_csv(os.path(outdir, 'lda_x_assess.csv'))
+        # todo: hack; remove once rlearn fixed
+        import glob
+        files = glob.glob(os.path.join(outdir, '*.csv'))
+        for filename in files:
+            os.unlink(filename)
 
-        for filename, df in enumerate(outputs):
-            df.to_csv(os.path(outdir, "%s.csv" % filename))
-
-        #TODO: eval if we really need to serialize avgp and rloadrank
+        dfunct.to_csv(os.path.join(outdir, 'lda_x_dfunct.csv'), index=False)
+        assess.to_csv(os.path.join(outdir, 'lda_x_assess.csv'), index=False)
